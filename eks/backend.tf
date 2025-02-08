8669c9c62fe08e46c0ca324a56d36ee97994e924
RahulVarne <rahulvarne16@gmail.com>
2025-02-08 18:55:14 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,6 @@ terraform {
     bucket         = "dev-aman-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
     encrypt        = true
   }
 }
